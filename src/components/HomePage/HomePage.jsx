8c1103e6e5b1ce30548394ed278e86268abef082
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-07-15 15:19:28 -0500

    users assessments and facilities are displayed on their home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,22 +2,51 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Sheet, Table } from '@mui/joy';
 import { DateTime } from 'luxon';
+import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+// import IconButton from '@mui/joy/IconButton';
+import Button from '@mui/joy/Button';
 
 function HomePage() {
   const reports = useSelector((store) => store.reports);
-  console.log('check reports', reports);
   const facilities = useSelector((store) => store.facilities);
-  console.log('check facilities', facilities);
+  const carbonTotal = useSelector((store) => store.carbon);
+  console.log('check carbon', carbonTotal);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: 'FETCH_USER_REPORTS' });
     dispatch({ type: 'FETCH_USER_FACILITIES' });
+    dispatch({ type: 'FETCH_CARBON' });
   }, []);
+
+  const deleteFacility = (facilityId) => {
+    console.log('delete facility was clicked, check id', facilityId);
+    dispatch({ type: 'DELETE_FACILITY', payload: facilityId });
+  };
+
   return (
     <div>
       <>
-        <h3>Total carbon footprint & energy costs card</h3>
+        <Box
+          sx={{
+            width: '100%',
+            maxWidth: 500,
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(240px, 1fr))',
+            gap: 2,
+          }}
+        >
+          <Card variant='outlined'>
+            <CardContent>
+              <Typography level='title-md'>Total carbon footprint: </Typography>
+              <Typography>Total energy cost #</Typography>
+            </CardContent>
+          </Card>
+        </Box>
         <h3>My assessments</h3>
         <Sheet sx={{ height: 200, overflow: 'auto' }}>
           <Table borderAxis='bothBetween' color='neutral' size='md' stickyFooter={false} stickyHeader variant='plain'>
@@ -39,7 +68,35 @@ function HomePage() {
             </tbody>
           </Table>
         </Sheet>
-        <h3>My facilities card</h3>
+        <h3>My facilities </h3>
+        <Sheet sx={{ height: 200, overflow: 'auto' }}>
+          <Table borderAxis='bothBetween' color='neutral' size='md' stickyFooter={false} stickyHeader variant='plain'>
+            <thead>
+              <tr>
+                <th style={{ width: '25%' }}>Facility</th>
+                <th>Address</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {facilities.map((facility) => (
+                <tr key={facility.id}>
+                  <td>{facility.name}</td>
+                  <td>
+                    {facility.address}, {facility.state}
+                  </td>
+                  <td>
+                    <Button>Edit</Button>
+                  </td>
+                  <td>
+                    <Button onClick={() => deleteFacility(facility.id)}>Delete</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Sheet>
       </>
     </div>
   );
--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import reports from './report.reducer';
 import facilities from './facility.reducer';
+import carbon from './carbon.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   reports,
   facilities,
+  carbon,
 });
 
 export default rootReducer;
