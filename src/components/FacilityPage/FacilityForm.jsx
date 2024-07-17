0a20d8647247b9b57354e4b2d697b0804b5dca12
Adam Troxell <adamtxl@hotmail.com>
2024-07-17 11:14:31 -0500

    Refactored Facility page modal to match Logans modal closer

--- src/components/FacilityPage/FacilityForm.jsx
+++ src/components/FacilityPage/FacilityForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Modal, Box, Typography, TextField, Button, Input, Select, FormControl, FormLabel, Option } from '@mui/joy';
+import { Modal, Typography, TextField, Button, Input, Select, FormControl, FormLabel, Option, ModalDialog, Grid, Box } from '@mui/joy';
 
 function FacilityForm() {
 	const [facilityInfo, setFacilityInfo] = useState({
@@ -16,7 +16,6 @@ function FacilityForm() {
 	});
 	const [open, setOpen] = useState(false);
 	const dispatch = useDispatch();
-	const [option, setOption] = useState('');
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
@@ -26,13 +25,12 @@ function FacilityForm() {
 		}));
 	};
 
-	// const handleOptions = (e) => {
-	// 	const { name, value } = e.target;
-	// 	setFacilityInfo((prevState) => ({
-	// 		...prevState,
-	// 		[name]: value,
-	// 	}));
-	// };
+	const handleSelectChange = (event, newValue) => {
+		setFacilityInfo((prevState) => ({
+			...prevState,
+			facilitySitDownRestaurant: newValue,
+		}));
+	};
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -45,7 +43,7 @@ function FacilityForm() {
 			building_age: parseFloat(facilityInfo.facilityBuildingAge, 10),
 			hours_of_operation: parseFloat(facilityInfo.facilityHoursOfOperation, 10),
 			weekly_customers: parseFloat(facilityInfo.facilityNumberOfGuests, 10),
-			sit_down: facilityInfo.facilitySitDownRestaurant === 'true',
+			sit_down: facilityInfo.facilitySitDownRestaurant === 'Yes',
 		};
 		try {
 			dispatch({ type: 'ADD_FACILITY', payload: submissionData });
@@ -70,148 +68,142 @@ function FacilityForm() {
 
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
+
 	return (
 		<>
 			<Button variant='outlined' onClick={handleOpen}>
 				Enter New Facility
 			</Button>
 			<Modal open={open} onClose={handleClose} aria-labelledby='modal-title' aria-describedby='modal-description'>
-				<Box
-					sx={{
-						position: 'absolute',
-						top: '50%',
-						left: '50%',
-						transform: 'translate(-50%, -50%)',
-						width: 400,
-						bgcolor: 'lightgreen',
-						border: '2px solid #000',
-						boxShadow: 24,
-						p: 4,
-					}}
-				>
+				<ModalDialog sx={{ width: '65vw' }}>
 					<Typography id='modal-title' level='h4'>
 						Add Facility Information
 					</Typography>
-					<Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-						<Input
-							margin='normal'
-							required
-							fullWidth
-							id='facilityName'
-							placeholder='Facility Name'
-							label='facilityName'
-							name='facilityName'
-							type='text'
-							value={facilityInfo.facilityName}
-							onChange={handleChange}
-							autoFocus
-						/>
-						<Input
-							margin='normal'
-							required
-							fullWidth
-							id='facilityAddress'
-							placeholder='Facility Address'
-							label='facilityAddress'
-							name='facilityAddress'
-							type='text'
-							value={facilityInfo.facilityAddress}
-							onChange={handleChange}
-							autoFocus
-						/>
-						<Input
-							margin='normal'
-							required
-							fullWidth
-							id='facilityState'
-							placeholder='State'
-							label='facilityState'
-							name='facilityState'
-							type='text'
-							value={facilityInfo.facilityState}
-							onChange={handleChange}
-							autoFocus
-						/>
-						<Input
-							margin='normal'
-							required
-							fullWidth
-							id='facilityZip'
-							placeholder='Zip'
-							label='facilityZip'
-							name='facilityZip'
-							type='text'
-							value={facilityInfo.facilityZip}
-							onChange={handleChange}
-							autoFocus
-						/>
-						<Input
-							margin='normal'
-							required
-							fullWidth
-							id='facilityYearsInBusiness'
-							placeholder='Years in Business'
-							label='facilityYearsInBusiness'
-							name='facilityYearsInBusiness'
-							type='number'
-							value={facilityInfo.facilityYearsInBusiness}
-							onChange={handleChange}
-							autoFocus
-						/>
-						<Input
-							margin='normal'
-							required
-							fullWidth
-							id='facilityBuildingAge'
-							placeholder='Building Age'
-							label='facilityBuildingAge'
-							name='facilityBuildingAge'
-							type='number'
-							value={facilityInfo.facilityBuildingAge}
-							onChange={handleChange}
-							autoFocus
-						/>
-						<Input
-							margin='normal'
-							required
-							fullWidth
-							id='facilityHoursOfOperation'
-							placeholder='Hours of Operation per week'
-							label='facilityHoursOfOperation'
-							name='facilityHoursOfOperation'
-							type='number'
-							value={facilityInfo.facilityHoursOfOperation}
-							onChange={handleChange}
-							autoFocus
-						/>
-						<Input
-							margin='normal'
-							required
-							fullWidth
-							id='facilityNumberOfGuests'
-							placeholder='Number of Guests per week'
-							label='facilityNumberOfGuests'
-							name='facilityNumberOfGuests'
-							type='number'
-							value={facilityInfo.facilityNumberOfGuests}
-							onChange={handleChange}
-							autoFocus
-						/>
-						{/* <FormControl>
-						<FormLabel>Select Option</FormLabel>
-						<Select
-							value={option}
-							onChange={handleOptions}
-							placeholder="Select Option"
-						>
-							<Option value="Yes">Yes</Option>
-							<Option value="No">No</Option>
-						</Select>
-					</FormControl> */}
-
-						<Button type='submit'>Submit</Button>
+					<Typography>Fill in the information from your equipment's data plate.</Typography>
+					<Box component='form' onSubmit={handleSubmit} noValidate>
+						<Grid container spacing={2}>
+							<Grid item xs={6}>
+								<FormControl fullWidth>
+									<FormLabel>Facility Name</FormLabel>
+									<Input
+										placeholder='Facility Name'
+										name='facilityName'
+										value={facilityInfo.facilityName}
+										onChange={handleChange}
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={6}>
+								<FormControl fullWidth>
+									<FormLabel>Facility Address</FormLabel>
+									<Input
+										placeholder='Facility Address'
+										name='facilityAddress'
+										value={facilityInfo.facilityAddress}
+										onChange={handleChange}
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={4}>
+								<FormControl fullWidth>
+									<FormLabel>State</FormLabel>
+									<Input
+										placeholder='State'
+										name='facilityState'
+										value={facilityInfo.facilityState}
+										onChange={handleChange}
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={4}>
+								<FormControl fullWidth>
+									<FormLabel>Zip</FormLabel>
+									<Input
+										placeholder='Zip'
+										name='facilityZip'
+										value={facilityInfo.facilityZip}
+										onChange={handleChange}
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={4}>
+								<FormControl fullWidth>
+									<FormLabel>Years in Business</FormLabel>
+									<Input
+										placeholder='Years in Business'
+										name='facilityYearsInBusiness'
+										type='number'
+										value={facilityInfo.facilityYearsInBusiness}
+										onChange={handleChange}
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={6}>
+								<FormControl fullWidth>
+									<FormLabel>Building Age</FormLabel>
+									<Input
+										placeholder='Building Age'
+										name='facilityBuildingAge'
+										type='number'
+										value={facilityInfo.facilityBuildingAge}
+										onChange={handleChange}
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={6}>
+								<FormControl fullWidth>
+									<FormLabel>Hours of Operation per week</FormLabel>
+									<Input
+										placeholder='Hours of Operation per week'
+										name='facilityHoursOfOperation'
+										type='number'
+										value={facilityInfo.facilityHoursOfOperation}
+										onChange={handleChange}
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={6}>
+								<FormControl fullWidth>
+									<FormLabel>Number of Guests per week</FormLabel>
+									<Input
+										placeholder='Number of Guests per week'
+										name='facilityNumberOfGuests'
+										type='number'
+										value={facilityInfo.facilityNumberOfGuests}
+										onChange={handleChange}
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={6}>
+								<FormControl fullWidth>
+									<FormLabel>Sit Down Restaurant</FormLabel>
+									<Select
+										value={facilityInfo.facilitySitDownRestaurant}
+										onChange={handleSelectChange}
+										placeholder='Select Option'
+										required
+									>
+										<Option value='Yes'>Yes</Option>
+										<Option value='No'>No</Option>
+									</Select>
+								</FormControl>
+							</Grid>
+							<Grid item xs={12}>
+								<Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+									Submit
+								</Button>
+							</Grid>
+						</Grid>
 					</Box>
-				</Box>
+				</ModalDialog>
 			</Modal>
 		</>
 	);
