bb4a503e5129372c3c16f33b947b4c370962dc67
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-07-29 11:55:08 -0500

    fix approveBy bug

--- src/components/ReportPages/EnergyReport/EnergyReport.jsx
+++ src/components/ReportPages/EnergyReport/EnergyReport.jsx
@@ -58,8 +58,8 @@ export default function EnergyReport() {
   const [deleteRecsToggle, setDeleteRecsToggle] = useState(false);
 
   const deleteRecommendation = (recommendationId) => {
-    console.log('in submitRecommendations, check rec id', recommendationId);
-    console.log('in submitRecommendations, check params id', params.id);
+    // console.log('in submitRecommendations, check rec id', recommendationId);
+    // console.log('in submitRecommendations, check params id', params.id);
     dispatch({
       type: 'DELETE_RECOMMENDATION',
       payload: { recommendationId, reportId: params.id },
@@ -94,13 +94,8 @@ export default function EnergyReport() {
 
   return (
     <Box sx={{ flex: 1 }}>
-      <h1 className='energy-report-header'>
-        {reportDetails.name} Energy Report
-      </h1>
-      <Grid
-        container
-        sx={{ justifyContent: 'space-between', mx: 6, mt: 3 }}
-      >
+      <h1 className='energy-report-header'>{reportDetails.name} Energy Report</h1>
+      <Grid container sx={{ justifyContent: 'space-between', mx: 6, mt: 3 }}>
         <Button
           onClick={navReports}
           sx={{
@@ -129,8 +124,7 @@ export default function EnergyReport() {
               </Button>
             ) : (
               <Typography sx={{ alignContent: 'center' }}>
-                Approved by: {reportDetails.username} on{' '}
-                {DateTime.now().toLocaleString(reportDetails.approvedAt)}
+                Approved by {reportDetails.approvedBy} on {DateTime.now().toLocaleString(reportDetails.approvedAt)}
               </Typography>
             )}
           </Box>
@@ -138,13 +132,10 @@ export default function EnergyReport() {
           <Box sx={{ display: 'flex', justifyContent: 'center' }}>
             {reportDetails.approved ? (
               <Typography sx={{ alignContent: 'center' }}>
-                Approved by: {reportDetails.username} on{' '}
-                {DateTime.now().toLocaleString(reportDetails.approvedAt)}
+                Approved by {reportDetails.approvedBy} on {DateTime.now().toLocaleString(reportDetails.approvedAt)}
               </Typography>
             ) : (
-              <Typography sx={{ alignContent: 'center' }}>
-                Pending Approval by RJ Energy
-              </Typography>
+              <Typography sx={{ alignContent: 'center' }}>Pending Approval by RJ Energy</Typography>
             )}
           </Box>
         )}
@@ -180,19 +171,13 @@ export default function EnergyReport() {
               <div>
                 <Typography level='title-lg'>Date Submitted: </Typography>
                 <Typography level='body-sm'>
-                  {DateTime.fromISO(reportDetails.date_submitted).toFormat(
-                    'MMMM dd, yyyy'
-                  )}
+                  {DateTime.fromISO(reportDetails.date_submitted).toFormat('MMMM dd, yyyy')}
                 </Typography>
                 <Typography level='title-lg'>Location Address:</Typography>
                 <Typography level='body-sm'>{reportDetails.address}</Typography>
                 <Typography level='title-lg'>Report Status:</Typography>
                 <Typography level='body-sm'>
-                  {reportDetails.approved ? (
-                    <td>Approved</td>
-                  ) : (
-                    <td>Pending Approval</td>
-                  )}
+                  {reportDetails.approved ? <td>Approved</td> : <td>Pending Approval</td>}
                 </Typography>
               </div>
             </Box>
@@ -218,12 +203,7 @@ export default function EnergyReport() {
             </Box>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <Typography level='body-sm'>
-                {reportDetails?.equipment
-                  ?.reduce(
-                    (sum, item) => sum + Number(item.carbon_footprint),
-                    0
-                  )
-                  .toFixed(3)}{' '}
+                {reportDetails?.equipment?.reduce((sum, item) => sum + Number(item.carbon_footprint), 0).toFixed(3)}{' '}
                 Tons/year
               </Typography>
             </Box>
@@ -233,20 +213,14 @@ export default function EnergyReport() {
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <Typography level='body-sm'>
                 $
-                {(
-                  reportDetails?.equipment?.reduce(
-                    (sum, item) => sum + Number(item.cost_per_month),
-                    0
-                  ) * 12
-                ).toFixed(2)}
+                {(reportDetails?.equipment?.reduce((sum, item) => sum + Number(item.cost_per_month), 0) * 12).toFixed(
+                  2
+                )}
               </Typography>
             </Box>
           </Card>
         </Box>
-        <Grid
-          container
-          sx={{ justifyContent: 'space-between', mx: 7, my: 2 }}
-        >
+        <Grid container sx={{ justifyContent: 'space-between', mx: 7, my: 2 }}>
           <Typography level='h3'>Equipment</Typography>
           {user.admin === true && (
             <Button
@@ -261,21 +235,11 @@ export default function EnergyReport() {
               Add Equiment
             </Button>
           )}
-          <EquipmentForm
-            open={open}
-            setOpen={setOpen}
-          />
+          <EquipmentForm open={open} setOpen={setOpen} />
         </Grid>
-        <Grid
-          container
-          sx={{ justifyContent: 'center' }}
-          spacing={2}
-        >
+        <Grid container sx={{ justifyContent: 'center' }} spacing={2}>
           {reportDetails.equipment?.map((e) => (
-            <Grid
-              xs={5.5}
-              key={e.id}
-            >
+            <Grid xs={5.5} key={e.id}>
               <ReportEquipmentCard e={e} />
             </Grid>
           ))}
@@ -315,36 +279,25 @@ export default function EnergyReport() {
               }}
             >
               <Typography level='title-lg'>Recommendations</Typography>
-              <IconButton
-                onClick={() => setDeleteRecsToggle(!deleteRecsToggle)}
-              >
+              <IconButton onClick={() => setDeleteRecsToggle(!deleteRecsToggle)}>
                 <EditIcon />
               </IconButton>
               <IconButton onClick={() => setAddRecToggle(!addRecToggle)}>
                 <AddCircleOutlineIcon />
               </IconButton>
             </Box>
-            <Box
-              sx={{ display: 'flex', alignItems: 'center', overflow: 'auto' }}
-            >
+            <Box sx={{ display: 'flex', alignItems: 'center', overflow: 'auto' }}>
               <Typography level='body-sm'>
                 {reportDetails?.recommendations?.map((recommendation) =>
                   deleteRecsToggle ? (
-                    <li
-                      className='recommendation-item'
-                      key={recommendation.id}
-                    >
-                      <IconButton
-                        onClick={() => deleteRecommendation(recommendation.id)}
-                      >
+                    <li className='recommendation-item' key={recommendation.id}>
+                      <IconButton onClick={() => deleteRecommendation(recommendation.id)}>
                         <ClearIcon />
                       </IconButton>
                       {recommendation.recommendation}
                     </li>
                   ) : (
-                    <li key={recommendation.id}>
-                      {recommendation.recommendation}
-                    </li>
+                    <li key={recommendation.id}>{recommendation.recommendation}</li>
                   )
                 )}
                 {addRecToggle ? (
@@ -358,10 +311,7 @@ export default function EnergyReport() {
                         value={newRec.recommendations}
                         onChange={handleNewRec}
                       />
-                      <Button
-                        type='submit'
-                        color='success'
-                      >
+                      <Button type='submit' color='success'>
                         Submit
                       </Button>
                     </Box>
@@ -407,9 +357,7 @@ export default function EnergyReport() {
             >
               <Typography level='title-lg'>Report Notes</Typography>
               {user.admin && (
-                <IconButton
-                  onClick={() => setEditNotesToggle(!editNotesToggle)}
-                >
+                <IconButton onClick={() => setEditNotesToggle(!editNotesToggle)}>
                   <EditIcon />
                 </IconButton>
               )}
@@ -446,10 +394,7 @@ export default function EnergyReport() {
                         })
                       }
                     />
-                    <Button
-                      color='success'
-                      type='submit'
-                    >
+                    <Button color='success' type='submit'>
                       Submit
                     </Button>
                   </Box>
